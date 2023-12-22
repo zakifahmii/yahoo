07da99a5939da29a8099dcaeb2010ce188667106
zakifahmi <73203819+zakifahmii@users.noreply.github.com>
2023-12-22 11:04:31 +0700

    Update Invoice.php

--- application/controllers/Invoice.php
+++ application/controllers/Invoice.php
@@ -1,15 +1,25 @@
 <?php
 
-class Invoice extends CI_Controller{
+class Invoice extends CI_Controller
+{
     public function index()
     {
         $this->load->model('ModelInvoice');
+
+        // Ambil data invoice dari model
         $data['invoice'] = $this->ModelInvoice->tampil_data();
+
+        // Hitung nilai total dari data invoice (contoh, di sini $total dihitung dari jumlah data invoice)
+        $data['total'] = count($data['invoice']);
+
+        // Load view invoice.php dengan data yang telah disiapkan
         $this->load->view('admin/header');
         $this->load->view('admin/sidebar');
         $this->load->view('admin/invoice', $data);
-        $this->load->view('admin/footer');  
+        $this->load->view('admin/footer');
     }
+
+
     public function detail($id_invoice)
     {
         $data['invoice'] = $this->ModelInvoice->ambil_id_invoice($id_invoice);
@@ -19,5 +29,4 @@ class Invoice extends CI_Controller{
         $this->load->view('admin/detail_invoice', $data);
         $this->load->view('admin/footer');
     }
-    
 }
